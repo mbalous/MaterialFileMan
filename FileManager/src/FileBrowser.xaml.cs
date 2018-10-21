c06f999502c9112a532eceb5f6753ff715cff059
Martin Balous <mbalous@outlook.com>
2018-10-22 00:15:10 +0200

    Part of FileBrowser implemented using DI and MVVM.

--- FileManager/src/FileBrowser.xaml.cs
+++ FileManager/src/FileBrowser.xaml.cs
@@ -14,34 +14,6 @@ namespace FileManager
     /// </summary>
     public partial class FileBrowser : UserControl
     {
-        private DirectoryInfo _currentDirectoryInfo;
-
-        public string CurrentPath
-        {
-            get { return this._currentDirectoryInfo.FullName; }
-            private set
-            {
-                this._currentDirectoryInfo = new DirectoryInfo(value);
-                this.TextBoxPath.Text = this._currentDirectoryInfo.FullName;
-                List<FileSystemInfo> fileSystemInfos = new List<FileSystemInfo>();
-                DirectoryInfo directoryInfo = new DirectoryInfo(this._currentDirectoryInfo.FullName);
-                fileSystemInfos.AddRange(directoryInfo.GetDirectories());
-                fileSystemInfos.AddRange(directoryInfo.GetFiles());
-
-                LinkedList<BrowserItem> systemInfoViews = new LinkedList<BrowserItem>();
-                if (CanGoUp())
-                {
-                    systemInfoViews.AddFirst(new GoUpItem());
-                }
-                foreach (FileSystemInfo systemInfoView in fileSystemInfos)
-                {
-                    systemInfoViews.AddLast(new FilesystemItem(systemInfoView));
-                }
-
-                this.ListBoxItems.ItemsSource = systemInfoViews;
-            }
-        }
-
         public double ElementSizing
         {
             get { return (double)GetValue(ElementSizingProperty); }
@@ -52,71 +24,13 @@ namespace FileManager
         public static readonly DependencyProperty ElementSizingProperty =
             DependencyProperty.Register("ElementSizing", typeof(double), typeof(FileBrowser), new PropertyMetadata(12d));
 
-        private FileBrowserViewModel _dataContext;
+        private FileBrowserViewModel _dataContext => this.DataContext as FileBrowserViewModel;
 
-        /// <summary>
-        /// Create new FileBrowser. 
-        /// Current directory is going to be used upon start.
-        /// </summary>
-        public FileBrowser() : this(Directory.GetCurrentDirectory())
-        {
-        }
-
-        public FileBrowser(string currentPath)
+        public FileBrowser()
         {
             InitializeComponent();
-            _dataContext = new FileBrowserViewModel();
-            this.CurrentPath = currentPath;
-        }
-
-        private void ButtonGoUp_Click(object sender, RoutedEventArgs e)
-        {
-            if (CanGoUp())
-            {
-                GoUp();
-            }
-        }
-
-        public bool CanGoUp()
-        {
-            return this._currentDirectoryInfo.Parent != null;
-        }
-
-        public void GoUp()
-        {
-            if (!CanGoUp())
-            {
-                throw new InvalidOperationException();
-            }
-
-            this.CurrentPath = this._currentDirectoryInfo.Parent.FullName;
-        }
-
-        private void ListBoxItems_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if (this.ListBoxItems.SelectedItem == null)
-            {
-                return;
-            }
-            if (this.ListBoxItems.SelectedItem is GoUpItem)
-            {
-                if (CanGoUp())
-                {
-                    GoUp();
-                }
-                return;
-            }
-
-            FilesystemItem selectedItem = (FilesystemItem)this.ListBoxItems.SelectedItem;
-            if (selectedItem.IsDirectory)
-            {
-                this.CurrentPath = selectedItem.FullName;
-            }
-            else
-            {
-                Process.Start(selectedItem.FullName);
-            }
-        }
+            this.DataContext = new FileBrowserViewModel();
+        }        
 
         private void TextBoxPath_KeyDown(object sender, KeyEventArgs e)
         {
@@ -125,14 +39,11 @@ namespace FileManager
                 case Key.Enter:
                     if (Directory.Exists(this.TextBoxPath.Text))
                     {
-                        this.CurrentPath = this.TextBoxPath.Text;
+                        _dataContext.CurrentPath = this.TextBoxPath.Text;
                     }
                     break;
                 case Key.Back:
-                    if (CanGoUp())
-                    {
-                        GoUp();
-                    }
+                    _dataContext.GoUpButtonClick.Execute();
                     break;
             }
         }
--- FileManager/src/FileBrowserViewModel.cs
+++ FileManager/src/FileBrowserViewModel.cs
@@ -1,10 +1,128 @@
-﻿namespace FileManager
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace FileManager
 {
-    public class FileBrowserViewModel
+    public class FileBrowserViewModel : ViewModelBase
     {
+        public string CurrentPath
+        {
+            get { return this._currentDirectoryInfo.FullName; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new InvalidOperationException("Directory can not be null.", new ArgumentNullException());
+                }
+
+                this._currentDirectoryInfo = new DirectoryInfo(value);
+                this.TextboxPath = this._currentDirectoryInfo.FullName;
+                List<FileSystemInfo> fileSystemInfos = new List<FileSystemInfo>();
+                DirectoryInfo directoryInfo = new DirectoryInfo(this._currentDirectoryInfo.FullName);
+                fileSystemInfos.AddRange(directoryInfo.GetDirectories());
+                fileSystemInfos.AddRange(directoryInfo.GetFiles());
+
+                LinkedList<BrowserItem> systemInfoViews = new LinkedList<BrowserItem>();
+                if (CanGoUp)
+                {
+                    systemInfoViews.AddFirst(new GoUpItem());
+                }
+                foreach (FileSystemInfo systemInfoView in fileSystemInfos)
+                {
+                    systemInfoViews.AddLast(new FilesystemItem(systemInfoView));
+                }
+
+                this.Items = systemInfoViews;
+                OnPropertyChanged();
+            }
+        }
+
+        private DirectoryInfo _currentDirectoryInfo;
+
+
         public FileBrowserViewModel()
         {
-
+            this.CurrentPath = Directory.GetCurrentDirectory();
+            this.GoUpButtonClick = ActionCommand.Create((o) =>
+            {
+                if (CanGoUp) { GoUp(); }
+            });
+            this.DataGridDoubleClick = ActionCommand.Create((o) =>
+            {
+                if (this.SelectedItem is GoUpItem)
+                {
+                    if (CanGoUp)
+                    {
+                        GoUp();
+                        return;
+                    }
+                }
+                else if (this.SelectedItem is FilesystemItem fsItem)
+                {
+                    if (fsItem.IsDirectory)
+                    {
+                        this.CurrentPath = fsItem.FullName;
+                    }
+                    else
+                    {
+                        Process.Start(fsItem.FullName);
+                    }
+                }
+                else
+                {
+                    throw new ApplicationException();
+                }                
+            });
         }
+
+        public bool CanGoUp => this._currentDirectoryInfo.Parent != null;
+
+        public void GoUp()
+        {
+            if (!CanGoUp) { throw new InvalidOperationException(); }
+
+            this.CurrentPath = this._currentDirectoryInfo.Parent.FullName;
+        }
+
+        #region Bindings 
+
+        public ActionCommand GoUpButtonClick { get; private set; }
+
+        public ActionCommand DataGridDoubleClick { get; private set; }
+
+        private LinkedList<BrowserItem> _items;
+        public LinkedList<BrowserItem> Items
+        {
+            get => _items; private set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private dynamic _selectedItem;
+        public dynamic SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _textboxPath;
+        public string TextboxPath
+        {
+            get => _textboxPath; set
+            {
+                _textboxPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
     }
 }
