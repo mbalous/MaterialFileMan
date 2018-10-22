f9a52f45d1a76190af0a452080a699c1e39af272
Martin Balous <mbalous@outlook.com>
2018-10-22 23:10:23 +0200

    FileBrowser icons are now slightly larger than the text.

---
+++ FileManager/src/IncrementConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows.Data;
+
+namespace FileManager
+{
+    class IncrementConverter : IValueConverter
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public object Convert(object value, Type targetType, dynamic parameter, CultureInfo culture)
+        {
+            if (value is double d)
+            {
+                return d + parameter;
+            }
+            else if (value is int i)
+            {
+                return i + parameter;
+            }
+            throw new ArgumentException(nameof(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
