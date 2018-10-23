9101077c8684294f7b926b3feeb32b3a5e2abd9c
Martin Balous <mbalous@outlook.com>
2018-10-23 11:38:31 +0200

    Added a test

---
+++ FileManager.Tests/FilesystemItemTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace FileManager.Tests
+{
+    [TestClass]
+    public class FilesystemItemTests
+    {
+        private DirectoryInfo _sampleDirectory;
+        private FileSystemInfo _sampleFile;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var tempDir = Path.GetTempPath();
+            this._sampleDirectory = Directory.CreateDirectory(Path.Combine(tempDir, "sampleDirectory"));
+            var stream = File.Create(Path.Combine(_sampleDirectory.FullName, "file.txt"));
+            stream.Dispose();
+            this._sampleFile = _sampleDirectory.GetFiles("file.txt")[0];
+        }
+
+        [TestMethod]
+        public void TestFileDetection()
+        {
+            FilesystemItem fsi = new FilesystemItem(_sampleDirectory);
+            Assert.IsTrue(fsi.IsDirectory);
+
+            fsi = new FilesystemItem(_sampleFile);
+            Assert.IsFalse(fsi.IsDirectory);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _sampleDirectory.Delete(true);
+        }
+    }
+}
---
+++ FileManager.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileManager.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileManager.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9780fbb-a001-4f8e-a958-380a7a3715cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
