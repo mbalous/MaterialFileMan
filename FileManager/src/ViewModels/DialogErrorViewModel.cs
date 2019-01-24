d05803b1d76139ccf7f1b3a3f4f10916c029d4e9
Martin Balous <mbalous@outlook.com>
2019-01-24 23:52:22 +0100

    Error dialog

---
+++ FileManager/src/ViewModels/DialogErrorViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace FileManager.ViewModels
+{
+    /// <summary>
+    /// ViewModel for <see cref="Views.DialogErrorView"/>
+    /// </summary>
+    class DialogErrorViewModel : ViewModelBase
+    {
+        private string _message;
+
+        public DialogErrorViewModel()
+        {
+            if ((bool)(DesignerProperties.IsInDesignModeProperty.GetMetadata(typeof(DependencyObject)).DefaultValue))
+            {
+                this.Message = "No application is associated with the specified file for this operation.";
+            }
+            
+        }
+
+        public DialogErrorViewModel(string message)
+        {
+            this.Message = message;
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                SetAndRaise(value, ref _message);
+            }
+        }
+    }
+}
---
+++ FileManager/src/Views/DialogErrorView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileManager.Views
+{
+    /// <summary>
+    /// Interaction logic for DialogErrorView.xaml
+    /// </summary>
+    public partial class DialogErrorView : UserControl
+    {
+        public DialogErrorView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogHost.CloseDialogCommand.Execute(null, this);
+        }
+    }
+}
