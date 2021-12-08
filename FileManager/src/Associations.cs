8e5f946b239eac31803abf1d146015d2a4c49879
Martin Balous <mbalous@outlook.com>
2021-12-08 19:21:52 +0100

    Improve file icon associations

--- FileManager/src/Associations.cs
+++ FileManager/src/Associations.cs
@@ -13,17 +13,21 @@ namespace FileManager
             Associations = new Dictionary<string, PackIcon>()
             {
                 { ".exe", new PackIcon() {Kind = PackIconKind.Application } },
-                { ".dll", new PackIcon() {Kind = PackIconKind.SettingsOutline } },
-                { ".jpg", new PackIcon() {Kind = PackIconKind.Image } },
-                { ".jpeg", new PackIcon() {Kind = PackIconKind.Image } },
-                { ".png", new PackIcon() {Kind = PackIconKind.Image } },
+                { ".dll", new PackIcon() {Kind = PackIconKind.FileCog } },
+                { ".jpg", new PackIcon() {Kind = PackIconKind.FileImage } },
+                { ".jpeg", new PackIcon() {Kind = PackIconKind.FileImage } },
+                { ".png", new PackIcon() {Kind = PackIconKind.FileImage } },
+                { ".webp", new PackIcon() {Kind = PackIconKind.FileImage } },
+                { ".bmp", new PackIcon() {Kind = PackIconKind.FileImage } },
                 { ".doc", new PackIcon() {Kind = PackIconKind.FileWord } },
                 { ".docx", new PackIcon() {Kind = PackIconKind.FileWord } },
                 { ".pdf", new PackIcon() {Kind = PackIconKind.FilePdfBox } },
-                { ".txt", new PackIcon() {Kind = PackIconKind.FileDocumentBox } },
+                { ".txt", new PackIcon() {Kind = PackIconKind.FileDocument } },
+                { ".log", new PackIcon() {Kind = PackIconKind.FileDocument } },
                 { ".rar", new PackIcon() {Kind = PackIconKind.ZipBox } },
                 { ".7z", new PackIcon() {Kind = PackIconKind.ZipBox } },
                 { ".zip", new PackIcon() {Kind = PackIconKind.ZipBox } },
+                { ".mp4", new PackIcon() {Kind = PackIconKind.FileVideo } },
             };
 
             DefaultIcon = new PackIcon() { Kind = PackIconKind.File };
