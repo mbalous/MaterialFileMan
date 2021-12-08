66a1e1d910a7bcd9c23ab0969c4825dc62ce7626
Martin Balous <mbalous@outlook.com>
2021-12-08 16:06:24 +0100

    Move SystemMenuWindow to controls

--- FileManager/src/Controls/SystemMenuWindow.cs
+++ FileManager/src/Controls/SystemMenuWindow.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 
-namespace FileManager
+namespace FileManager.Controls
 {
     public class SystemMenuWindow : Window
     {
--- FileManager/src/Views/MainWindow.xaml.cs
+++ FileManager/src/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using FileManager.Controls;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace FileManager.Views
