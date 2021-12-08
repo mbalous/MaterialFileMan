b3680251bb9694c7eb8e7fb230058d96709997a9
Martin Balous <mbalous@outlook.com>
2021-12-08 18:58:54 +0100

    Move BindableBase out of viewmodels folder

--- FileManager/src/BindableBase.cs
+++ FileManager/src/BindableBase.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace FileManager.ViewModels
+namespace FileManager
 {
     public abstract class BindableBase : INotifyPropertyChanged
     {
