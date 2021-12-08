029ff289cb6517673e2db9c2167ce832dc79098c
Martin Balous <mbalous@outlook.com>
2021-12-08 18:51:45 +0100

    Refactor viewmodel

--- FileManager/src/ViewModels/MainWindowViewModel.cs
+++ FileManager/src/ViewModels/MainWindowViewModel.cs
@@ -1,49 +1,15 @@
 ﻿using MaterialDesignThemes.Wpf;
 using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Windows;
 
 namespace FileManager.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         #region Commands
-#pragma warning disable CS0649
 
-        public ActionCommand _decreaseTextSizeCommand;
-        public ActionCommand DecreaseTextSizeCommand
-        {
-            get
-            {
-                return _decreaseTextSizeCommand ?? ActionCommand.Create((o) =>
-                {
-                    this.FontSize -= 2;
-                });
-            }
-        }
-
-        private ActionCommand _increaseTextSizeCommand;
-        public ActionCommand IncreaseTextSizeCommand
-        {
-            get
-            {
-                return _increaseTextSizeCommand ?? ActionCommand.Create((o) =>
-                {
-                    this.FontSize += 2;
-                });
-            }
-        }
-
-        private ActionCommand _switchColorSchemeCommand;
-        public ActionCommand SwitchColorSchemeCommand
-        {
-            get
-            {
-                return _switchColorSchemeCommand ?? ActionCommand.Create(SwitchColorScheme);
-            }
-        }
+        public ActionCommand<int> ChangeTextSizeCommand { get; }
+        public ActionCommand SwitchColorSchemeCommand { get; }
 
         private double _fontSize = 12;
         public double FontSize
@@ -54,39 +20,37 @@ namespace FileManager.ViewModels
             }
             set
             {
-                SetAndRaise(value, ref _fontSize);
+                SetAndRaise(Math.Abs(value), ref _fontSize);
                 Debug.WriteLine($"FontSize set to {this.FontSize}");
             }
         }
 
-#pragma warning restore CS0649
         #endregion
 
         public MainWindowViewModel()
         {
             this._paletteHelper = new PaletteHelper();
+
+            this.ChangeTextSizeCommand = new ActionCommand<int>(ChangeTextSize);
+            this.SwitchColorSchemeCommand = new ActionCommand(SwitchColorScheme);
+        }
+
+        private void ChangeTextSize(int param)
+        {
+            this.FontSize += param;
         }
 
         private readonly PaletteHelper _paletteHelper;
 
+        private bool _isDark = false;
+
         private void SwitchColorScheme(object obj)
         {
-            // This code retreives the current color scheme
-            System.Collections.Generic.IEnumerable<ResourceDictionary> existingResourceDictionary = Application.Current.Resources.MergedDictionaries.Where(rd => rd.Source != null);
-            ResourceDictionary target = existingResourceDictionary.SingleOrDefault(rd =>
-                Regex.Match(rd.Source.OriginalString, @"(\/MaterialDesignThemes.Wpf;component\/Themes\/MaterialDesignTheme\.)((Light)|(Dark))",
-                RegexOptions.Compiled).Success);
-
-            if (existingResourceDictionary == null)
-            {
-                throw new ApplicationException($"{nameof(existingResourceDictionary)} equals null.");
-            }
-
-            // If the current collor scheme is set to dark,
-            // then the method 'SetLightDark' will swap it for light.
-            bool isDark = target.Source.ToString().ToLower().Contains("dark");
-            var theme = this._paletteHelper.GetTheme();
-            //this._paletteHelper.SetLightDark(!isDark);            
+            ITheme theme = this._paletteHelper.GetTheme();
+            IBaseTheme baseTheme = _isDark ?  new MaterialDesignLightTheme() : new MaterialDesignDarkTheme();
+            _isDark = !_isDark;
+            theme.SetBaseTheme(baseTheme);
+            _paletteHelper.SetTheme(theme);
         }
     }
 }
